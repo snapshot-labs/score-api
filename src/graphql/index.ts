3a8e53c8e12b9279e4ec1d6a032ec05c5e5206fd
bonustrack <fabien@bonustrack.co>
2021-06-17 20:59:37 +0700

    Update endpoint

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -24,7 +24,7 @@ const server = new ApolloServer({
     shareEnabled: true,
     tabs: [
       {
-        endpoint: './graphql',
+        endpoint: 'https://score.snapshot.org/graphql',
         query
       }
     ]
