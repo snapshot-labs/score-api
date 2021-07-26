f4b916d224ea48c7c7cb13e0424f970a0cfc304f
Chaitanya <yourchaitu@gmail.com>
2021-07-26 17:13:33 +0530

    Strategies Object API

--- src/scores.ts
+++ src/scores.ts
@@ -1,5 +1,5 @@
 import { createHash } from 'crypto';
-import snapshot from '@snapshot-labs/snapshot.js';
+import * as snapshot from '@snapshot-labs/snapshot.js';
 import { get, set } from './aws';
 
 export const blockNumByNetwork = {};
--- src/strategies.ts
+++ src/strategies.ts
@@ -1,11 +1,37 @@
-// import snapshot from '@snapshot-labs/snapshot.js';
+import requireContext from 'node-require-context';
+import fs from 'fs';
+import path from 'path';
 
-export default {
-  'erc20-balance-of': {
-    id: 'erc20-balance-of',
-    author: 'bonustrack',
-    version: '0.1.0',
-    about: '...from README.md',
-    examples: []
-  }
-}
+const strategiesPath = path.resolve(__dirname, '../node_modules/@snapshot-labs/snapshot.js/dist/strategies');
+const strategiesPathSrc = path.resolve(__dirname, '../node_modules/@snapshot-labs/snapshot.js/dist/strategies');
+
+const requireFile = requireContext(strategiesPath, true, /index\.js$/);
+const requireExamplesJson = requireContext(strategiesPathSrc, true, /examples\.json$/);
+
+export default Object.fromEntries(
+  requireFile
+    .keys()
+    .filter(fileName => fileName !== './index.js')
+    .map(fileName => {
+      const key = fileName
+        .replace('/index.js', '')
+        .replace('\\index.js', '')
+        .replace(strategiesPath, '')
+        .replace('/', '')
+        .replace('\\', '');
+      const strategy = requireFile(fileName);
+      fileName = fileName.replace('dist', 'src');
+      strategy.key = key;
+      try {
+        strategy.about = fs.readFileSync(fileName.replace('index.js', 'README.md')).toString();
+      } catch (error) {
+        strategy.about = '';
+      }
+      try {
+        strategy.examples = requireExamplesJson(fileName.replace('index.js', 'examples.json'));
+      } catch (error) {
+        strategy.examples = null;
+      }
+      return [key, strategy];
+    })
+);
