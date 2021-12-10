e8667c948f0ddd68a2f28dc2f8691ca7ea5291df
Chaitanya <yourchaitu@gmail.com>
2021-12-10 11:30:53 +0530

    Rate limit

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import api from './api';
+import rateLimit from './rateLimit';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json({ limit: '8mb' }));
 app.use(bodyParser.urlencoded({ limit: '8mb', extended: false }));
 app.use(cors({ maxAge: 86400 }));
+app.use(rateLimit);
 app.use('/api', api);
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,18 @@
+import { createHash } from 'crypto';
+
 export function clone(item) {
   return JSON.parse(JSON.stringify(item));
+}
+
+export function sendError(res, description, status = 500) {
+  return res.status(status).json({
+    error: 'unauthorized',
+    error_description: description
+  });
+}
+
+export function sha256(str) {
+  return createHash('sha256')
+    .update(str)
+    .digest('hex');
 }
