94eee56e57929a6eb493df2962817bcce32fa3fc
Chaitanya <yourchaitu@gmail.com>
2021-12-14 23:21:39 +0530

    Fix

--- src/cache.ts
+++ src/cache.ts
@@ -6,6 +6,7 @@ const memCache = new cache.Cache();
 const eventEmitter = new events.EventEmitter();
 
 const cacheTime = 30000; // ms
+
 export const cacheMiddleware = (req, res, next) => {
   const key = createHash('sha256')
     .update(JSON.stringify(req.body))
@@ -18,21 +19,22 @@ export const cacheMiddleware = (req, res, next) => {
     memCache.put(key, cacheContent, cacheTime);
     res.send(JSON.parse(cacheContent));
   }
-  // If cache is pending, wait for cache event and respond with it
-  else if (cacheContent && cacheContent === 'pending') {
-    eventEmitter.on(key, () => {
-      const cacheContent = memCache.get(key);
-      res.send(JSON.parse(cacheContent));
+  // If cache is pending, listen to cache event and respond with data
+  else if (cacheContent === 'pending') {
+    eventEmitter.on(key, function(data) {
+      if (!res.headersSent) {
+        res.send(JSON.parse(data));
+      }
     });
   }
-  // If cache is not available, send the request to next function to get the data, store it in cache and call the event
+  // If cache is not available, send the request to next function to get the data, store it in cache and trigger the event
   else {
     memCache.put(key, 'pending', cacheTime);
     res.sendResponse = res.send;
     res.send = body => {
       memCache.put(key, body, cacheTime);
-      eventEmitter.emit(key);
       res.sendResponse(body);
+      eventEmitter.emit(key, body);
     };
     next();
   }
