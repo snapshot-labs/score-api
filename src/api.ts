cac5da0626a8f5e86d71f6799f48986ffa133e3e
ChaituVR <ChaituVR@users.noreply.github.com>
2022-04-03 10:24:25 +0000

    Automated lint

--- src/api.ts
+++ src/api.ts
@@ -28,7 +28,11 @@ router.post('/scores', async (req, res) => {
   strategies = formatStrategies(strategies, network);
   const strategyNames = strategies.map(strategy => strategy.name);
 
-  if (['revotu.eth', 'biswap-org.eth'].includes(space) || strategyNames.includes('pod-leader') || strategies.length === 0)
+  if (
+    ['revotu.eth', 'biswap-org.eth'].includes(space) ||
+    strategyNames.includes('pod-leader') ||
+    strategies.length === 0
+  )
     return res.status(500).json({
       jsonrpc: '2.0',
       error: {
