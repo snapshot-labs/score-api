36b28babe0eadcdf89534d8db4fe251be4c6030c
bonustrack <fabien@bonustrack.co>
2022-07-16 00:26:06 +0700

    Add JSONRPC endpoint with method to get VP

--- src/utils.ts
+++ src/utils.ts
@@ -52,3 +52,23 @@ export function formatStrategies(strategies: Array<any> = [], network) {
     .map(sortObjectByParam)
     .slice(0, 8);
 }
+
+export function rpcSuccess(res, result, id) {
+  res.json({
+    jsonrpc: '2.0',
+    result,
+    id
+  });
+}
+
+export function rpcError(res, code, e, id) {
+  res.status(code).json({
+    jsonrpc: '2.0',
+    error: {
+      code,
+      message: 'unauthorized',
+      data: e
+    },
+    id
+  });
+}
