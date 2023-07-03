edf01f245ce9af3f3ceb8a1204a7ba3985c4ecbf
Wan <495709+wa0x6e@users.noreply.github.com>
2023-07-03 15:05:29 +0900

    fix: return a JSON-RPC formatted 404 error (#784)
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import rpc from './rpc';
+import { rpcError } from './utils';
 
 const app = express();
 const PORT = process.env.PORT ?? 3003;
@@ -10,5 +11,9 @@ app.use(express.json({ limit: '8mb' }));
 app.use(express.urlencoded({ limit: '8mb', extended: false }));
 app.use(cors({ maxAge: 86400 }));
 app.use('/', rpc);
+
+app.use((req, res) => {
+  rpcError(res, 404, {}, req.body.id);
+});
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
