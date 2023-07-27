0ee3f2b9580800f9d33f5dad9b14830c8e58fd4e
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-07-27 17:20:26 +0900

    chore: setup and add list of tests to implement

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+
+  // An array of regexp pattern strings used to skip coverage collection
+  coveragePathIgnorePatterns: ['/node_modules/', '<rootDir>/dist/', '<rootDir>/test/fixtures/'],
+
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-node-single-context',
+  setupFiles: ['dotenv/config'],
+  //setupFilesAfterEnv: ['<rootDir>src/setupTests.ts'],
+  moduleFileExtensions: ['js', 'ts'],
+  testPathIgnorePatterns: ['dist/'],
+  verbose: true
+};
---
+++ test/e2e/scores.test.ts
@@ -0,0 +1,3 @@
+describe('scores', () => {
+  it.todo('trigger a scores computation');
+});
