227b38280048b41593c1c8db14ac02bf6312ae81
Wan <495709+wa0x6e@users.noreply.github.com>
2023-07-29 17:33:40 +0900

    chore: use node 16 (#814)
    * chore: use node 16
    
    * chore: update github actions task to use node 16
    
    * chore: fix node version in CI
    
    * chore: remove redundant task

--- test/e2e/get_vp.test.ts
+++ test/e2e/get_vp.test.ts
@@ -1,3 +1,13 @@
+import request from 'supertest';
+
 describe('getVp', () => {
+  describe('when the address params is missing', () => {
+    it('returns a 500 error', async () => {
+      const response = await request(process.env.HOST).post('/').send({ method: 'get_vp' });
+
+      expect(response.status).toEqual(500);
+    });
+  });
+
   it.todo('returns the voting power');
 });
