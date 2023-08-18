00e4d8c7b3acc7dabc79a92a2bfbd5de8190dd4a
Dmytro Tolok <tolok.d@gmail.com>
2023-08-18 10:31:14 +0200

    feat(scores): change request deduplicator (#833)
    * test(scores): add unit tests for scores functions
    
    * test(scores): add case for future block scores
    
    * refactor(deduplication): change way to deduplicate requests
    
    * fix(lint): fix lint errors
    
    * fix(unit): separate configs for e2e and unit tests
    
    * fix(e2e): add path to tests
    
    * fix(coverate): separate coverage folders for unit and e2e
    
    * refactor(unit): move unit tests to a separate folder

---
+++ test/unit/scores.test.ts
@@ -0,0 +1,101 @@
+import scores from '../../src/scores';
+import { get, set } from '../../src/aws';
+import { getBlockNum, sha256 } from '../../src/utils';
+import snapshot from '@snapshot-labs/strategies';
+
+jest.mock('../../src/utils');
+jest.mock('../../src/aws');
+jest.mock('@snapshot-labs/strategies');
+
+describe('scores function', () => {
+  const mockArgs = {
+    space: '',
+    strategies: ['strategy1'],
+    network: 'ethereum',
+    addresses: ['0x123'],
+    snapshot: '12345'
+  };
+  const key = 'mockKey';
+  const mockScores = { data: 'scores' };
+
+  beforeEach(() => {
+    (sha256 as jest.Mock).mockReturnValue(key);
+    jest.clearAllMocks();
+  });
+
+  it('should deduplicate requests', async () => {
+    (snapshot.utils.getScoresDirect as jest.Mock).mockResolvedValue(mockScores);
+    const firstCall = scores(null, mockArgs);
+    const secondCall = scores(null, mockArgs);
+
+    const firstResult = await firstCall;
+    const secondResult = await secondCall;
+
+    expect(firstResult).toEqual(secondResult);
+    expect(snapshot.utils.getScoresDirect).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return cached results', async () => {
+    process.env.AWS_REGION = 'us-west-1';
+    (get as jest.Mock).mockResolvedValue(mockScores);
+
+    const result = await scores(null, mockArgs);
+
+    expect(result).toEqual({
+      cache: true,
+      scores: mockScores,
+      state: 'final'
+    });
+    expect(get).toHaveBeenCalledWith(key);
+  });
+
+  it('should set cache if not cached before', async () => {
+    process.env.AWS_REGION = 'us-west-1';
+    (get as jest.Mock).mockResolvedValue(null); // Not in cache
+    (snapshot.utils.getScoresDirect as jest.Mock).mockResolvedValue(mockScores);
+
+    await scores(null, mockArgs);
+
+    expect(set).toHaveBeenCalledWith(key, mockScores);
+  });
+
+  it('should return uncached results when cache is not needed', async () => {
+    process.env.AWS_REGION = 'us-west-1';
+    (get as jest.Mock).mockResolvedValue(null); // Not in cache
+    (snapshot.utils.getScoresDirect as jest.Mock).mockResolvedValue(mockScores);
+    const result = await scores(null, { ...mockArgs, snapshot: 'latest' }); // "latest" should bypass cache
+
+    expect(result).toEqual({
+      cache: false,
+      scores: mockScores,
+      state: 'pending'
+    });
+    expect(set).not.toHaveBeenCalled();
+  });
+
+  it("shouldn't return cached results when cache is not available", async () => {
+    process.env.AWS_REGION = '';
+    (snapshot.utils.getScoresDirect as jest.Mock).mockResolvedValue(mockScores);
+    const result = await scores(null, mockArgs);
+
+    expect(result).toEqual({
+      cache: false,
+      scores: mockScores,
+      state: 'final'
+    });
+    expect(get).not.toHaveBeenCalled();
+  });
+
+  it('should restrict block number by `latest`', async () => {
+    (snapshot.utils.getScoresDirect as jest.Mock).mockResolvedValue(mockScores);
+    (getBlockNum as jest.Mock).mockResolvedValue('100');
+
+    const result = await scores(null, { ...mockArgs, snapshot: '99999999' });
+
+    expect(result).toEqual({
+      cache: false,
+      scores: mockScores,
+      state: 'pending'
+    });
+  });
+});
