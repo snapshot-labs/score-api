4fe251234c336f77c88763328346feb827e08173
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-08-28 13:55:58 +0900

    fix: fix missing import

--- src/metrics.ts
+++ src/metrics.ts
@@ -1,4 +1,4 @@
-import init from '@snapshot-labs/snapshot-metrics';
+import init, { client } from '@snapshot-labs/snapshot-metrics';
 import { Express } from 'express';
 
 export default function initMetrics(app: Express) {
