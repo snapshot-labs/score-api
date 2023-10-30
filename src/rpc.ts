f04cece064a79488044beba337504a2048123d3c
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-10-30 18:05:48 +0700

    fix: too many requests error message should return 429 code

--- src/rpc.ts
+++ src/rpc.ts
@@ -33,7 +33,7 @@ router.post('/', async (req, res) => {
 
   if (method === 'get_vp') {
     if (params.space && disabled.includes(params.space))
-      return rpcError(res, 500, 'too many requests', null);
+      return rpcError(res, 429, 'too many requests', null);
 
     try {
       const response: any = await serve(JSON.stringify(params), getVp, [params]);
