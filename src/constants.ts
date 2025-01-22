eaafc7a6cf6ad01700cbd3975bd804523d1e1ea1
Chaitanya <yourchaitu@gmail.com>
2025-01-22 23:30:12 +0530

    chore: increment cache key (#1255)

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000';
 export const MAX_STRATEGIES = 10;
 export const APP_NAME = 'score-api';
-export const AWS_CACHE_KEY = '4';
+export const AWS_CACHE_KEY = '5';
