e9cc9baff0410b2ab2577687ddcef7b81d2fde0d
Massimiliano <94859244+BromeRST@users.noreply.github.com>
2025-09-16 19:39:01 +0100

    fix: test issues

--- src/strategies/strategies/aavegotchi-agip-37-gltr-staked-lp/index.ts
+++ src/strategies/strategies/aavegotchi-agip-37-gltr-staked-lp/index.ts
@@ -23,31 +23,31 @@ export async function strategy(
 
   options.ghstFudAddress =
     options.ghstFudAddress || '0xeae2fB93e291C2eB69195851813DE24f97f1ce71';
-  options.ghstFudPoolId = options.ghstFudPoolId || 1;
+  options.ghstFudPoolId = options.ghstFudPoolId || 0;
 
   options.ghstFomoAddress =
     options.ghstFomoAddress || '0x62ab7d558A011237F8a57ac0F97601A764e85b88';
-  options.ghstFomoPoolId = options.ghstFomoPoolId || 2;
+  options.ghstFomoPoolId = options.ghstFomoPoolId || 1;
 
   options.ghstAlphaAddress =
     options.ghstAlphaAddress || '0x0Ba2A49aedf9A409DBB0272db7CDF98aEb1E1837';
-  options.ghstAlphaPoolId = options.ghstAlphaPoolId || 3;
+  options.ghstAlphaPoolId = options.ghstAlphaPoolId || 2;
 
   options.ghstKekAddress =
     options.ghstKekAddress || '0x699B4eb36b95cDF62c74f6322AaA140E7958Dc9f';
-  options.ghstKekPoolId = options.ghstKekPoolId || 4;
-  // TODO: Update ghstUsdc
-  options.ghstUsdcAddress =
-    options.ghstUsdcAddress || '0x096c5ccb33cfc5732bcd1f3195c13dbefc4c82f4';
-  options.ghstUsdcPoolId = options.ghstUsdcPoolId || 5;
+  options.ghstKekPoolId = options.ghstKekPoolId || 3;
+  // // TODO: Update ghstUsdc
+  // options.ghstUsdcAddress =
+  //   options.ghstUsdcAddress || '0x096c5ccb33cfc5732bcd1f3195c13dbefc4c82f4';
+  // options.ghstUsdcPoolId = options.ghstUsdcPoolId || 5;
 
   options.ghstWethAddress =
     options.ghstWethAddress || '0x0DFb9Cb66A18468850d6216fCc691aa20ad1e091';
-  options.ghstWethPoolId = options.ghstWethPoolId || 6;
+  options.ghstWethPoolId = options.ghstWethPoolId || 4;
 
   options.ghstGltrAddress =
     options.ghstGltrAddress || '0xa83b31D701633b8EdCfba55B93dDBC202D8A4621';
-  options.ghstGltrPoolId = options.ghstGltrPoolId || 7;
+  options.ghstGltrPoolId = options.ghstGltrPoolId || 5;
 
   const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
 
@@ -81,8 +81,8 @@ export async function strategy(
       [options.ghstAddress, 'balanceOf', [options.ghstKekAddress]],
       [options.ghstGltrAddress, 'totalSupply', []],
       [options.ghstAddress, 'balanceOf', [options.ghstGltrAddress]],
-      [options.ghstUsdcAddress, 'totalSupply', []],
-      [options.ghstAddress, 'balanceOf', [options.ghstUsdcAddress]],
+      // [options.ghstUsdcAddress, 'totalSupply', []],
+      // [options.ghstAddress, 'balanceOf', [options.ghstUsdcAddress]],
       [options.ghstWethAddress, 'totalSupply', []],
       [options.ghstAddress, 'balanceOf', [options.ghstWethAddress]],
       ...slicedStakedQueries[0]
@@ -128,19 +128,19 @@ export async function strategy(
       res[lpTokensStartIndex + 9],
       res[lpTokensStartIndex + 8]
     ),
-    ghstUsdcLp: tokensPerUni(
+    // ghstUsdcLp: tokensPerUni(
+    //   res[lpTokensStartIndex + 11],
+    //   res[lpTokensStartIndex + 10]
+    // ),
+    ghstWethLp: tokensPerUni(
       res[lpTokensStartIndex + 11],
       res[lpTokensStartIndex + 10]
-    ),
-    ghstWethLp: tokensPerUni(
-      res[lpTokensStartIndex + 13],
-      res[lpTokensStartIndex + 12]
     )
   };
 
   const entries = {};
   for (let addressIndex = 0; addressIndex < addresses.length; addressIndex++) {
-    const i = addressIndex + 14;
+    const i = addressIndex + 12;
     const tokens = {
       staked: {
         ghstFudLp:
@@ -158,9 +158,9 @@ export async function strategy(
         ghstGltrLp:
           Number(res[i]._info[options.ghstGltrPoolId].userBalance.toString()) /
           1e18,
-        ghstUsdcLp:
-          Number(res[i]._info[options.ghstUsdcPoolId].userBalance.toString()) /
-          1e18,
+        // ghstUsdcLp:
+        //   Number(res[i]._info[options.ghstUsdcPoolId].userBalance.toString()) /
+        //   1e18,
         ghstWethLp:
           Number(res[i]._info[options.ghstWethPoolId].userBalance.toString()) /
           1e18
@@ -174,7 +174,7 @@ export async function strategy(
         ghstAlphaLp: tokens.staked.ghstAlphaLp * lpTokensPerUni.ghstAlphaLp,
         ghstKekLp: tokens.staked.ghstKekLp * lpTokensPerUni.ghstKekLp,
         ghstGltrLp: tokens.staked.ghstGltrLp * lpTokensPerUni.ghstGltrLp,
-        ghstUsdcLp: tokens.staked.ghstUsdcLp * lpTokensPerUni.ghstUsdcLp,
+        // ghstUsdcLp: tokens.staked.ghstUsdcLp * lpTokensPerUni.ghstUsdcLp,
         ghstWethLp: tokens.staked.ghstWethLp * lpTokensPerUni.ghstWethLp
       }
     };
