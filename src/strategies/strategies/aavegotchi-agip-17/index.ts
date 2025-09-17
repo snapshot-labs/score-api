45b9075e684b984d0c04cc1d6baac469f3d74e17
Massimiliano <94859244+BromeRST@users.noreply.github.com>
2025-09-17 11:05:02 +0100

    [aavegotchi-wagmi-guild] update/aavegotchi-strategies-for-base (#1380)
    * feat: add base to aavegotchi strategies
    
    * fix: remove deprecated polygon parts
    
    * fix: add new wearables
    
    * feat: update addresses for gltr staking strategy
    
    * fix: test issues
    
    * Update src/strategies/strategies/aavegotchi-agip-37-gltr-staked-lp/manifest.json
    
    * Update src/strategies/strategies/aavegotchi-agip-17/manifest.json
    
    * Update src/strategies/strategies/aavegotchi-wagmi-guild/manifest.json
    
    * Update src/strategies/strategies/aavegotchi-agip/manifest.json
    
    ---------
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

--- src/strategies/strategies/aavegotchi-agip-17/index.ts
+++ src/strategies/strategies/aavegotchi-agip-17/index.ts
@@ -1,7 +1,7 @@
 import { subgraphRequest } from '../../utils';
 
 const AAVEGOTCHI_SUBGRAPH_URL = {
-  137: 'https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/aavegotchi-core-matic/api'
+  8453: 'https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/aavegotchi-core-base/api'
 };
 
 const maxResponsePerQuery = 1000;
