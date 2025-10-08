033b6bda133791782fb8311da365cc2809526826
Chaitanya <yourchaitu@gmail.com>
2025-10-08 21:12:34 +0530

    Apply suggestion from @ChaituVR

--- src/strategies/strategies/evolabs-dao/index.ts
+++ src/strategies/strategies/evolabs-dao/index.ts
@@ -3,8 +3,6 @@ import { getAddress } from '@ethersproject/address';
 import { Multicaller } from '../../utils';
 import { getDelegations } from '../../utils/delegation';
 
-export const author = 'sketchminds';
-export const version = '1.0.0';
 
 // ABI for Soulbound Token (ERC-721 standard)
 const sbtAbi = [
