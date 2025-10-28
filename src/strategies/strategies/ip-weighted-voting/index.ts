b13b3ac07052eb35e9d36981c32526f43ea3bfd3
Spablob <tiagonunespereira@gmail.com>
2025-10-28 09:57:18 +0000

    add remaining APIs

--- src/strategies/strategies/ip-weighted-voting/index.ts
+++ src/strategies/strategies/ip-weighted-voting/index.ts
@@ -16,28 +16,41 @@ export async function strategy(
   snapshot
 ) {
   // Create provider directly
-  let customProvider;
-  let API_URL;
+  let CUSTOM_PROVIDER;
+  let API_CHAIN_URL;
+  let API_TOTAL_SUPPLY_URL;
+  let minimumTotalTokenSupply;
+
   if (network.toString() === '1514') {
-    API_URL = 'https://mainnet.storyrpc.io';
-    customProvider = new StaticJsonRpcProvider(
-      'https://mainnet.storyrpc.io', // RPC URL for Story mainnet
+    API_CHAIN_URL = 'http://127.0.0.1:1317';
+    CUSTOM_PROVIDER = new StaticJsonRpcProvider(
+      'http://127.0.0.1:8545', // RPC URL for Story mainnet
       1514 // chain ID for Story mainnet
     );
+    API_TOTAL_SUPPLY_URL =
+      'https://mainnet-circulation-supply.storyapis.com/history/total-supply?block=';
+    minimumTotalTokenSupply = 1000000000; // 1 billion
   } else if (network.toString() === '1315') {
-    API_URL = 'http://127.0.0.1:1317';
-    customProvider = new StaticJsonRpcProvider(
+    API_CHAIN_URL = 'http://127.0.0.1:1317';
+    CUSTOM_PROVIDER = new StaticJsonRpcProvider(
       'http://127.0.0.1:8545', // RPC URL for Aeneid
       1315 // chain ID for Aeneid
     );
+    API_TOTAL_SUPPLY_URL =
+      'https://circulation-supply.storyapis.com/history/total-supply?block=';
+    minimumTotalTokenSupply = 950000000; // 950 million
   }
 
-  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+  const blockTag =
+    typeof snapshot === 'number'
+      ? snapshot
+      : await CUSTOM_PROVIDER.getBlockNumber();
+  // console.log("blockTag", blockTag);
 
   // Get native balances for all addresses
-  const response = await multicall(
+  const nativeBalances = await multicall(
     network,
-    customProvider,
+    CUSTOM_PROVIDER,
     abi,
     addresses.map((address: any) => [
       networks[network].multicall,
@@ -47,11 +60,11 @@ export async function strategy(
     { blockTag }
   );
 
-  // Get staked balances for all addresses
+  // Get staked balances for validator addresses
   const stakedBalances = await Promise.all(
     addresses.map(async address => {
       const responseStaked = await fetch(
-        `${API_URL}/staking/delegators/${address}/total_staked_token`,
+        `${API_CHAIN_URL}/staking/validators/${address}`,
         {
           headers: {
             'X-Block-Height': blockTag.toString()
@@ -59,19 +72,65 @@ export async function strategy(
         }
       );
       const data = await responseStaked.json();
-      return parseFloat(data.msg.staked_token);
+
+      // Check if the response indicates that the address is an address that is not a validator
+      // and if so return 0 for the staked balance of non-validators
+      if (
+        data.code === 500 &&
+        data.error &&
+        data.error.includes('rpc error: code = NotFound desc = validator')
+      ) {
+        return 0;
+      }
+
+      return parseFloat(data.msg.validator.tokens);
     })
   );
 
+  // Get the total token supply (staked and unstaked)
+  const totalTokenSupplyResponse = await fetch(
+    `${API_TOTAL_SUPPLY_URL}${blockTag.toString()}`
+  );
+  const totalTokenSupplyData = await totalTokenSupplyResponse.json();
+  const totalTokenSupply = parseFloat(totalTokenSupplyData.result);
+  if (totalTokenSupply < minimumTotalTokenSupply) {
+    throw new Error(
+      'Total token supply is less than minimum total token supply'
+    );
+  }
+
+  // Get the total staked token supply
+  const totalStakedSupplyResponse = await fetch(
+    `${API_CHAIN_URL}/staking/total_staked_token`,
+    {
+      headers: {
+        'X-Block-Height': blockTag.toString()
+      }
+    }
+  );
+  const totalStakedSupplyData = await totalStakedSupplyResponse.json();
+  const totalStakedSupply = parseFloat(
+    formatUnits(totalStakedSupplyData.msg.total_staked_token.toString(), 9)
+  ); // staked balance comes from API comes in wei (10^9) and we remove those 9 significant digits
+
+  const totalVotingPower =
+    (totalTokenSupply - totalStakedSupply) * 1 + totalStakedSupply * 1.25;
+
+  //console.log("totalVotingPower", totalVotingPower, "totalTokenSupply", totalTokenSupply, "totalStakedSupply", totalStakedSupply);
+
   return Object.fromEntries(
-    response.map((value, i) => {
-      const ethBalance = parseFloat(formatUnits(value.toString(), 18)); // native balance comes from RPC with 0 decimals and we convert to achieve 18 decimals
+    nativeBalances.map((value, i) => {
+      const nativeBalance = parseFloat(formatUnits(value.toString(), 18)); // native balance comes from RPC with 0 decimals and we convert to achieve 18 decimals
       const stakedBalance = parseFloat(
         formatUnits(stakedBalances[i].toString(), 9)
-      ); // staked balance comes from API comes in wei (10^9) and we remove those 9 significant digits to achieve 18 decimals
-      const votingPower = ethBalance + 1.25 * stakedBalance;
+      ); // staked balance comes from API comes in wei (10^9) and we remove those 9 significant digits
+      const votingPower = nativeBalance * 1 + stakedBalance * 1.25;
 
-      return [addresses[i], votingPower];
+      const votingPowerPercentage = (votingPower * 100) / totalVotingPower;
+
+      // console.log(addresses[i], "nativeBalance", nativeBalance, "stakedBalance", stakedBalance, "votingPower", votingPower, "votingPowerPercentage", votingPowerPercentage);
+
+      return [addresses[i], votingPowerPercentage];
     })
   );
 }
