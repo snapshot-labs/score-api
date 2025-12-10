b5d6a640982b20747cc3eab6e86fce460d723ae8
Coderdan <dmathieuva@gmail.com>
2025-12-10 13:02:35 +0800

    chore: update subgraph url (#1413)

--- src/strategies/strategies/aavegotchi-wagmi-guild/index.ts
+++ src/strategies/strategies/aavegotchi-wagmi-guild/index.ts
@@ -2,7 +2,7 @@ import { formatUnits } from '@ethersproject/units';
 import { subgraphRequest } from '../../utils';
 
 const AAVEGOTCHI_SUBGRAPH_URL = {
-  8453: 'https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/aavegotchi-core-base/api'
+  8453: 'https://api.goldsky.com/api/public/project_cmh3flagm0001r4p25foufjtt/subgraphs/aavegotchi-core-base/prod/gn'
 };
 
 const itemPriceParams = {
