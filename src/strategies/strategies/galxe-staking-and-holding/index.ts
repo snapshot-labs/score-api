100511a5340d8604e6ceccc7ce551eacb5881105
xmujx <117618147+xmujx@users.noreply.github.com>
2026-05-12 20:05:59 +0800

    [galxe-staking-and-holding] feat: add strategy (#1435)
    * [galxe-staking-and-holding] feat: add strategy
    
    Voting power = staked amount + wallet token balance.
    Uses two parallel Multicaller queries for staking (getStakeAmount)
    and token holding (balanceOf).
    
    * [galxe-staking-and-holding] chore: update author

---
+++ src/strategies/strategies/galxe-staking-and-holding/index.ts
@@ -0,0 +1,49 @@
+import { BigNumberish } from '@ethersproject/bignumber';
+import { Multicaller } from '../../utils';
+import { formatUnits } from '@ethersproject/units';
+
+const stakingAbi = [
+  'function getStakeAmount(address) external view returns (uint256)'
+];
+
+const tokenAbi = [
+  'function balanceOf(address account) external view returns (uint256)'
+];
+
+export async function strategy(
+  space,
+  network,
+  provider,
+  addresses,
+  options,
+  snapshot
+): Promise<Record<string, number>> {
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+
+  const stakingMulti = new Multicaller(network, provider, stakingAbi, {
+    blockTag
+  });
+  const tokenMulti = new Multicaller(network, provider, tokenAbi, {
+    blockTag
+  });
+
+  addresses.forEach(address => {
+    stakingMulti.call(address, options.stakingAddress, 'getStakeAmount', [
+      address
+    ]);
+    tokenMulti.call(address, options.tokenAddress, 'balanceOf', [address]);
+  });
+
+  const [stakingResult, tokenResult]: [
+    Record<string, BigNumberish>,
+    Record<string, BigNumberish>
+  ] = await Promise.all([stakingMulti.execute(), tokenMulti.execute()]);
+
+  return Object.fromEntries(
+    addresses.map(address => [
+      address,
+      parseFloat(formatUnits(stakingResult[address], options.decimals)) +
+        parseFloat(formatUnits(tokenResult[address], options.decimals))
+    ])
+  );
+}
